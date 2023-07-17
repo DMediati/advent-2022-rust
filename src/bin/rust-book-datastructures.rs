c1fe7272e4fe06b33c4a9ad2411fd8fcf5dca5b9
David Mediati <davidmediati@gmail.com>
2023-07-17 01:34:45 -0400

    Rust pig-latin translator and started type interface practice question

--- src/bin/rust-book-datastructures.rs
+++ src/bin/rust-book-datastructures.rs
@@ -1,5 +1,6 @@
 use std::{env, collections::HashMap};
 
+
 fn median_and_mode(mut input: Vec<i32>) -> (f32, Vec<i32>) {
     let n = input.len();
     assert!(n > 0, "Input length must be greater than 0!");
@@ -36,6 +37,39 @@ fn median_and_mode(mut input: Vec<i32>) -> (f32, Vec<i32>) {
     return (median, mode);
 }
 
+
+fn pig_latin(input: &str) -> String {
+    let mut output: String = String::from("");
+    const VOWELS: &[char] = &['a', 'e', 'i', 'o', 'u'];
+    for word in input.split_whitespace() {
+        if !output.is_empty() {
+            output += " ";
+        }
+
+        if word.is_empty() {
+            continue;
+        }
+
+        let first_char = word.chars().next().unwrap();
+        let translated_word: String = if VOWELS.contains(&first_char) {
+            format!("{}{}", word ,"-hay")
+        } else {
+           let remaining_word: String = word.chars().skip(1).take(word.len()).collect();
+           format!("{}-{}ay", remaining_word, &first_char)
+        };
+        output += &translated_word;
+    }
+
+    return output;
+}
+
+
+fn run_type_interface() {
+    println!("Welcome to the Rust Employee Typing Interface!");
+
+}
+
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     dbg!(&args);
@@ -49,5 +83,13 @@ fn main() {
         println!("{:?}", &input);
         let (median, mode) = median_and_mode(input);
         println!("Median: {median}, Mode: {:?}", mode);
+    } else if &args[1] == "p" {
+        println!("Pig-Latin:");
+        let input = &args[2];
+        println!("Original : {:?}", &input);
+        let output = pig_latin(input);
+        println!("Pig-Latin: {}", output);
+    } else if &args[1] == "t" {
+        run_type_interface();
     }
 }
